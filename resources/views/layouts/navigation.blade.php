d0d34d4df9d7d77c0b000daf378b985926e33b0a
talithaolga <111202214110@mhs.dinus.ac.id>
2023-06-09 00:21:24 +0700

    build: initial commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('article.index')" :active="request()->routeIs('article')">
+                        {{ __('Article') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,15 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/article', [ArticleController::class, 'index'])->name('article.index');
+    Route::get('/article/create', [ArticleController::class, 'create'])->name('article.create');
+    Route::post('/article/create', [ArticleController::class, 'store'])->name('article.store'); // untuk menambahkan data
+    Route::get('/article/{id}', [ArticleController::class, 'edit'])->name('article.edit');
+    Route::patch('/article/{id}', [ArticleController::class, 'update'])->name('article.update'); 
+    Route::delete('/article/{id}', [ArticleController::class, 'destroy'])->name('article.destroy');
 });
 
 require __DIR__.'/auth.php';

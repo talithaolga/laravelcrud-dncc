9b59b70910b7ad0580f47d7136630393865518a6
talithaolga <111202214110@mhs.dinus.ac.id>
2023-06-09 00:22:30 +0700

    build: initial commit

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,12 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        return view('article.index');
+        return view('article.index', [
+            'articles' => Article::get()
+        ]);
+    }
+
+    public function create()
+    {
+        return view('article.form');
+    }
+
+    public function store(Request $request)
+    {
+        $inputs = $request->only(['title', 'description']);
+        $create = Article::create($inputs);
+
+        if($create) {
+            // add flash for the success notification
+            session()->flash('notif.success', 'User created successfully!');
+            return redirect()->route('article.index');
+        }
+
+        return abort(500); //jika gagal
+    }
+
+    public function edit($id)
+    {
+        $article = Article::find($id);
+        return view('article.form', [
+            'article' => $article
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->only(['title', 'description']);
+        $article = Article::find($id);
+        $update = $article -> update($inputs);
+
+        if($update) {
+            return redirect()->route('article.index');
+        }
+
+        return abort(500); //jika gagal
+    }
+
+    public function destroy($id)
+    {
+        $article = Article::find($id);
+        $delete = $article -> delete();
+
+        if($delete) {
+            return redirect()->route('article.index');
+        }
+
+        return abort(500); //jika gagal
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,6 +23,12 @@
                 <header class="bg-white shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
+
+                        @if (Session::has('notif.success'))
+                        <div class="bg-blue-300 mt-2 p-4">
+                            <span class="text-white">{{ Session::get('notif.success') }}</span>
+                        </div>
+                        @endif
                     </div>
                 </header>
             @endif
